d19052d019b75b7d4e3e5caf9d98ffc8b88faad6
Kevin Noseworthy <kevin.noseworthy@stjoseph.com>
2021-09-29 09:09:46 -0400

    new script setup

---
+++ PhotoShop/server.jsx
@@ -0,0 +1,14 @@
+#target "photoshop";
+
+
+#include "alphalaydowns4x5AlignBottom.jsx";
+
+app.displayDialogs = DialogModes.NO;
+app.preferences.rulerUnits = Units.PIXELS;
+
+
+
+var a = alphalaydownalignbottom();
+
+
+
