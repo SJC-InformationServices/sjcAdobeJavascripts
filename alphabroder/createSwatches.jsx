c01142174553dc52f7ca04df8a9ce7c98aeb7138
Kevin <kevin.noseworthy@stjoseph.com>
2022-07-11 16:43:14 -0400

    swatches

--- alphabroder/createSwatches.jsx
+++ alphabroder/createSwatches.jsx
@@ -1,5 +1,49 @@
 #target "indesign";
 
-var swatches = app.activeDocument.swatches;
 
-var file = File.selectDialog("Select Swatch Json File");
+var txt= "";
+var f = File.openDialog("Select Swatch Json File","*.json");
+f.open("r");
+while(!f.eof)
+{
+    txt+=f.readln();
+}
+f.close();
+//eval("{"+txt+"}"); // return undefined
+
+var obj = eval('{'+txt+'}');
+var doc = app.activeDocument;
+var cs;
+
+for(var i=0;i<obj.length;i++)
+{
+    try {
+    var sw = obj[i];
+    
+    var csa= sw.space;
+    
+    if(sw["space"]["Value"]=="CMYK")
+    {
+    cs = ColorSpace.CMYK;
+    }else{
+    cs = ColorSpace.RGB;
+    }
+    n = sw["{Name}"];
+    var nums=[];
+    var v = sw.color_values.split(",");
+    for(var a=0;a<v.length;a++){
+        nums.push(Math.round(v[a]));
+    }
+    var m = ColorModel.PROCESS;
+    var vals={"colorValue":nums,"space":cs,"name":n,"label":n,"model":m};
+          
+            try {
+                var c = doc.colors.add(vals);
+            var t = doc.tints.add(vals.name);
+            }catch(e){
+                
+            }
+        }catch(e){}
+                
+}
+alert('done');
