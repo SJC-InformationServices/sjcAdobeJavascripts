805ad954d1f8ee0a0472f8a2e18eb1dd0eabd153
Kevin <kevin.noseworthy@stjoseph.com>
2022-10-05 10:04:57 -0400

    add date time logging

--- PhotoShop/server.jsx
+++ PhotoShop/server.jsx
@@ -2,6 +2,15 @@
 app.displayDialogs = DialogModes.NO;
 #include "\\lib\\hotfolder.jsx";
 
+let current = new Date();
+let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+let dateTime = cDate + ' ' + cTime;
+
+var tmpF = File("tmplog.log");
+	tmpF.open("a");
+    tmpF.writeln("Start: " + dateTime);
+    tmpF.close();
 try {
     
     var source = $.evalFile(File($.getenv('sjcHotFolders')));
@@ -26,5 +35,13 @@ var tmpF = File("tmplog.log");
          tmpF.writeln(e.message);
          tmpF.close();
 }
+let current = new Date();
+let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+let dateTime = cDate + ' ' + cTime;
+var tmpF = File("tmplog.log");
+	tmpF.open("a");
+    tmpF.writeln("End: " + dateTime);
+    tmpF.close();
 
 /*executeAction(app.charIDToTypeID('quit'),undefined,DialogModes.No);*/
