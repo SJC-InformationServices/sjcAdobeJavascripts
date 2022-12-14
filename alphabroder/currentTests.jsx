dcd6f455e02c0812f24803353b4a150110afea0b
Kevin <kevin.noseworthy@stjoseph.com>
2022-12-14 14:44:08 -0500

    Updated cropAndCenter3500x5250jpgBkgroud

--- alphabroder/currentTests.jsx
+++ alphabroder/currentTests.jsx
@@ -1,84 +1,119 @@
+function magicWand (x, y, t, a, c, s) {
+    try{
+    if (arguments.length < 2) return; // make sure have x,y
+    if (undefined == t) var t = 35; // set defaults of optional arguments
+    if (undefined == a) var a = true;
+    if (undefined == c) var c = false;
+    if (undefined == s) var s = false;
+    var desc = new ActionDescriptor();
+    var ref = new ActionReference();
+    ref.putProperty(charIDToTypeID('Chnl'), charIDToTypeID('fsel'));
+    desc.putReference(charIDToTypeID('null'), ref);
+    var positionDesc = new ActionDescriptor();
+    positionDesc.putUnitDouble(charIDToTypeID('Hrzn'), charIDToTypeID('#Rlt'), x); // in pixels
+    positionDesc.putUnitDouble(charIDToTypeID('Vrtc'), charIDToTypeID('#Rlt'), y);
+    desc.putObject(charIDToTypeID('T   '), charIDToTypeID('Pnt '), positionDesc);
+    desc.putInteger(charIDToTypeID('Tlrn'), t); // tolerance
+    desc.putBoolean(charIDToTypeID('Mrgd'), s); // sample all layers
+    if (!c) desc.putBoolean(charIDToTypeID("Cntg"), false); //  contiguous
+    desc.putBoolean(charIDToTypeID('AntA'), a); // anti-alias
+    executeAction(charIDToTypeID('setd'), desc, DialogModes.NO);
+    }catch(e){
+        return e;
+    }
+}
+function getCropDimensions(){
+    for(var y=0;y<app.activeDocument.pathItems.length;y++)
+    {
+        p=app.activeDocument.pathItems[y];
+        if(p.name == "Path 1")
+        {
+            p.makeSelection(1, 1, SelectionType.REPLACE);
+        }
+    }
+    
+var crop = app.activeDocument.selection.bounds.join("||").split("||");
+return crop;
+}
 var fw = 3500;
 var fh = 5250;
 var padding = 150;
 var minH = fh - (padding * 2);
 var minW = fw - (padding * 2);
+var outFolder=Folder("/C/Users/KevinNoseworthy/Desktop/New folder/out");
 
 var doc = app.activeDocument;
 var w = parseFloat(doc.width);
 var h = parseFloat(doc.height);
 
-var outFolder=Folder("/C/Users/KevinNoseworthy/Desktop/New folder/out");
+var als = doc.artLayers;
+for (var ii = 0; ii < als.length; ii++) {
+var al = als[ii];
+if (al.isBackgroundLayer || al.name=="Background") {
+    al.remove();
+}
+}
+   
+var crop = getCropDimensions();
 
+var objWidth = parseFloat(crop[2].replace("px",""))-parseFloat(crop[0].replace("px",""));
+var objHeight = parseFloat(crop[3].replace("px",""))-parseFloat(crop[1].replace("px",""));
 
-var als = app.activeDocument.artLayers;
-       /* for (var ii = 0; ii < als.length; ii++) {
-            var al = als[ii];
-            if (al.isBackgroundLayer) {
-                al.isBackgroundLayer=false;
-            }
-        }*/
-       doc.flatten(); 
-       for (var ii = 0; ii < als.length; ii++) {
-        var al = als[ii];
-        if (al.isBackgroundLayer) {
-            al.isBackgroundLayer=false;
-        }
-    }
-        
-        try {
-            var path = app.activeDocument.pathItems.getByName("Path 1");
-            path.makeSelection(1, 1, SelectionType.Replace);
-            doc.crop(doc.selection.bounds);
-            path.makeSelection(1,1, SelectionType.Replace);
-            doc.selection.invert();
-            doc.selection.clear();
-        }catch(e){
-            
-            try {
-            for(var y=0;y<app.activeDocument.pathItems.length;y++)
-            {
-                var p=app.activeDocument.pathItems[y];
-                if(p.name == "Path 1")
-                {
-                p.makeSelection(1, 1, SelectionType.REPLACE);
-                doc.crop(doc.selection.bounds);
-                p.makeSelection(1,1,SelectionType.REPLACE);
-                doc.selection.invert();
-                doc.selection.clear();
-                }
-            }}
-            catch(e){
-                //app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-            }
-        }
-       
-        try {
-            var doc = app.activeDocument;
-            
-            if (doc.width > doc.height) {
-                doc.resizeImage(minW + "px");
-                if (doc.height > parseInt(minH)) {
-                    doc.resizeImage(null, minH + "px");
-                }
-            } else {
-                doc.resizeImage(null, minH + "px");
-                if (doc.width > parseInt(minW)) {
-                    doc.resizeImage(minW);
-                }
-            }
-           
-            doc.resizeCanvas(fw, fh, AnchorPosition.MIDDLECENTER);
-        } catch (e) {
-            //app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-        }
-        
+if (objWidth > objHeight)
+{
+    var ratio = minW/objWidth;
+    doc.resizeImage(w*ratio+"px");
+} else {
+    var ratio = minH/objHeight;
+    doc.resizeImage(null,h*ratio+"px");
+}
+var cropb = getCropDimensions();
 
-        var nf = File(outFolder +"\\"+ app.activeDocument.name.split(".")[0]+".png");
-        exportOptions = new ExportOptionsSaveForWeb();
-                exportOptions.format = SaveDocumentType.PNG;
-                exportOptions.PNG8 = false; // false = PNG-24
-                exportOptions.transparency = true; // true = transparent
-                exportOptions.interlaced = false; // true = interlacing on
-                exportOptions.includeProfile = true; // false = don't embedd ICC profile
-                app.activeDocument.exportDocument(nf, ExportType.SAVEFORWEB, exportOptions,Extension.LOWERCASE);
+var nObjWidth = parseFloat(cropb[2].replace("px",""))-parseFloat(cropb[0].replace("px",""));
+var nObjHeight = parseFloat(cropb[3].replace("px",""))-parseFloat(cropb[1].replace("px",""));
+
+var nw = parseFloat(doc.width);
+var nh = parseFloat(doc.height);
+
+//Enough Width
+var eW =(parseFloat(cropb[2].replace("px",""))+parseFloat(cropb[0].replace("px",""))+padding)-nw;
+//Enough Height
+var eH =(parseFloat(cropb[3].replace("px",""))+parseFloat(cropb[1].replace("px",""))+padding)-nh;
+
+if(eW > 0 || eH > 0)
+{
+ var resizeHeight = eH>0?nh+eH:nh;
+ var resizeWidth = eW>0?nw+eW:nw;
+   
+ doc.resizeCanvas(resizeWidth+"px",resizeHeight+"px",AnchorPosition.TOPLEFT);
+}
+var cropc = getCropDimensions();
+var nCrop = [
+    parseFloat(cropc[0].replace("px",""))-padding+" px",
+    parseFloat(cropc[1].replace("px",""))-padding+" px",
+    parseFloat(cropc[2].replace("px",""))+padding+" px",
+    parseFloat(cropc[3].replace("px",""))+padding+" px",
+];
+doc.selection.deselect();
+//alert(cropc.join('\r\n')+"\r\n"+[doc.width,doc.height].join("\r\n")+"\r\n"+nCrop.join("\r\n"));
+doc.crop(nCrop);
+doc.resizeCanvas(fw,fh,AnchorPosition.MIDDLECENTER);
+var fillLayer = als.add();
+fillLayer.name="Fill";
+doc.selection.selectAll();
+var colorRef=new SolidColor();
+colorRef.cmyk.cyan="8";
+colorRef.cmyk.magenta="6";
+colorRef.cmyk.yellow="6";
+colorRef.cmyk.black="0";
+doc.selection.fill(colorRef);
+fillLayer.move(als[als.length-1],ElementPlacement.PLACEAFTER);
+doc.flatten();    
+
+            var nf = File(outFolder +"\\"+ app.activeDocument.name.split(".")[0]+".jpg");
+            var jpgSave = new JPEGSaveOptions();
+                jpgSave.embedColorProfile = true;
+                jpgSave.formatOptions = FormatOptions.STANDARDBASELINE;
+                jpgSave.matte = MatteType.NONE;
+                jpgSave.quality = 12;
+                app.activeDocument.saveAs(nf, jpgSave, true, Extension.LOWERCASE);
--- canadiantire/cropAndCenter3500x5250jpgBkgroud.jsx
+++ canadiantire/cropAndCenter3500x5250jpgBkgroud.jsx
@@ -1,151 +1,142 @@
-
-function cdnTire3500jpgBkgrd(){
-    function magicWand (x, y, t, a, c, s) {
-        try{
-        if (arguments.length < 2) return; // make sure have x,y
-        if (undefined == t) var t = 35; // set defaults of optional arguments
-        if (undefined == a) var a = true;
-        if (undefined == c) var c = false;
-        if (undefined == s) var s = false;
-        var desc = new ActionDescriptor();
-        var ref = new ActionReference();
-        ref.putProperty(charIDToTypeID('Chnl'), charIDToTypeID('fsel'));
-        desc.putReference(charIDToTypeID('null'), ref);
-        var positionDesc = new ActionDescriptor();
-        positionDesc.putUnitDouble(charIDToTypeID('Hrzn'), charIDToTypeID('#Rlt'), x); // in pixels
-        positionDesc.putUnitDouble(charIDToTypeID('Vrtc'), charIDToTypeID('#Rlt'), y);
-        desc.putObject(charIDToTypeID('T   '), charIDToTypeID('Pnt '), positionDesc);
-        desc.putInteger(charIDToTypeID('Tlrn'), t); // tolerance
-        desc.putBoolean(charIDToTypeID('Mrgd'), s); // sample all layers
-        if (!c) desc.putBoolean(charIDToTypeID("Cntg"), false); //  contiguous
-        desc.putBoolean(charIDToTypeID('AntA'), a); // anti-alias
-        executeAction(charIDToTypeID('setd'), desc, DialogModes.NO);
-        }catch(e){
+function cdnTire3500jpgBkgrd() {
+    function magicWand(x, y, t, a, c, s) {
+        try {
+            if (arguments.length < 2) return; // make sure have x,y
+            if (undefined == t) var t = 35; // set defaults of optional arguments
+            if (undefined == a) var a = true;
+            if (undefined == c) var c = false;
+            if (undefined == s) var s = false;
+            var desc = new ActionDescriptor();
+            var ref = new ActionReference();
+            ref.putProperty(charIDToTypeID('Chnl'), charIDToTypeID('fsel'));
+            desc.putReference(charIDToTypeID('null'), ref);
+            var positionDesc = new ActionDescriptor();
+            positionDesc.putUnitDouble(charIDToTypeID('Hrzn'), charIDToTypeID('#Rlt'), x); // in pixels
+            positionDesc.putUnitDouble(charIDToTypeID('Vrtc'), charIDToTypeID('#Rlt'), y);
+            desc.putObject(charIDToTypeID('T   '), charIDToTypeID('Pnt '), positionDesc);
+            desc.putInteger(charIDToTypeID('Tlrn'), t); // tolerance
+            desc.putBoolean(charIDToTypeID('Mrgd'), s); // sample all layers
+            if (!c) desc.putBoolean(charIDToTypeID("Cntg"), false); //  contiguous
+            desc.putBoolean(charIDToTypeID('AntA'), a); // anti-alias
+            executeAction(charIDToTypeID('setd'), desc, DialogModes.NO);
+        } catch (e) {
             return e;
         }
+    }
+
+    function getCropDimensions() {
+        for (var y = 0; y < app.activeDocument.pathItems.length; y++) {
+            p = app.activeDocument.pathItems[y];
+            if (p.name == "Path 1") {
+                p.makeSelection(1, 1, SelectionType.REPLACE);
+            }
+        }
+
+        var crop = app.activeDocument.selection.bounds.join("||").split("||");
+        return crop;
     }
     var fw = 3500;
     var fh = 5250;
     var padding = 75;
     var minH = fh - (padding * 2);
     var minW = fw - (padding * 2);
-    
-    var inFolder =  Folder("\\\\10.3.0.39\\Canadian Tire\\hotfolder\\3500x5250_png-jpg\\In");
+
+    var inFolder = Folder("\\\\10.3.0.39\\Canadian Tire\\hotfolder\\3500x5250_png-jpg\\In");
     var outFolder = Folder("\\\\10.3.0.39\\Canadian Tire\\hotfolder\\3500x5250_png-jpg\\OUT");
     /*var inFolder = Folder.selectDialog("Select Source Folder");
     var outFolder = Folder.selectDialog("Select Out Folder");*/
-    
-    
+
+
     var files = inFolder.getFiles(/\.(psd|tif|jpg|)$/i);
-    
+
     for (var i = 0; i < files.length; i++) {
         try {
-    
+
             f = files[i];
+            f.copy(outFolder + "\\" + f.name);
             app.open(f);
             var doc = app.activeDocument;
             var w = parseFloat(doc.width);
             var h = parseFloat(doc.height);
+
+
             var als = doc.artLayers;
-        for (var ii = 0; ii < als.length; ii++) {
-            var al = als[ii];
-            if (al.isBackgroundLayer || al.name=="Background") {
-                al.remove();
+            for (var ii = 0; ii < als.length; ii++) {
+                var al = als[ii];
+                if (al.isBackgroundLayer || al.name == "Background") {
+                    al.remove();
+                }
             }
-        }
 
-    try {
-        var mw = magicWand(5, 5, 1, true, true, true);
-        doc.selection.invert();
-    for(var y=0;y<app.activeDocument.pathItems.length;y++)
-    {
-        p=app.activeDocument.pathItems[y];
-        if(p.name == "Path 1")
-        {
-        p.makeSelection(1, 1, SelectionType.EXTEND);
-        }
-    }}
-    catch(e){
-        
-    }
+            var crop = getCropDimensions();
 
-var crop = app.activeDocument.selection.bounds.join(",").split(",");
+            var objWidth = parseFloat(crop[2].replace("px", "")) - parseFloat(crop[0].replace("px", ""));
+            var objHeight = parseFloat(crop[3].replace("px", "")) - parseFloat(crop[1].replace("px", ""));
+            var ratio;
+            if (objWidth > objHeight) {
+                ratio = minW / objWidth;
+                doc.resizeImage(w * ratio + "px");
+            } else {
+                ratio = minH / objHeight;
+                doc.resizeImage(null, h * ratio + "px");
+            }
+            var cropb = getCropDimensions();
 
-var objWidth = parseFloat(crop[2].replace("px",""))-parseFloat(crop[0].replace("px",""));
-var objHeight = parseFloat(crop[3].replace("px",""))-parseFloat(crop[1].replace("px",""));
+            var nObjWidth = parseFloat(cropb[2].replace("px", "")) - parseFloat(cropb[0].replace("px", ""));
+            var nObjHeight = parseFloat(cropb[3].replace("px", "")) - parseFloat(cropb[1].replace("px", ""));
 
-if (objWidth > objHeight)
-{
-    var ratio = minW/objWidth;
-    doc.resizeImage(w*ratio+"px");
-} else {
-    var ratio = minH/objHeight;
-    doc.resizeImage(null,h*ratio+"px");
-}
+            var nw = parseFloat(doc.width);
+            var nh = parseFloat(doc.height);
 
-var newBounds = [];
-var str = [w,h,objWidth,objHeight,minW,minH,ratio];
-var mw = magicWand(5, 5, 1, true, true, true);
-doc.selection.invert();
+            //Enough Width
+            var eW = (parseFloat(cropb[2].replace("px", "")) + parseFloat(cropb[0].replace("px", "")) + padding) - nw;
+            //Enough Height
+            var eH = (parseFloat(cropb[3].replace("px", "")) + parseFloat(cropb[1].replace("px", "")) + padding) - nh;
 
-for(var y=0;y<app.activeDocument.pathItems.length;y++)
-{
-p=app.activeDocument.pathItems[y];
-if(p.name == "Path 1")
-{
-p.makeSelection(1, 1, SelectionType.EXTEND);
+            if (eW > 0 || eH > 0) {
+                var resizeHeight = eH > 0 ? nh + eH : nh;
+                var resizeWidth = eW > 0 ? nw + eW : nw;
 
-}
-}
-var nCrop = app.activeDocument.selection.bounds.join(",").split(",");
-var nBounds = [
-    parseFloat(nCrop[0].replace("px",""))-padding,
-    parseFloat(nCrop[1].replace("px",""))-padding,
-    parseFloat(nCrop[2].replace("px",""))+padding,
-    parseFloat(nCrop[3].replace("px",""))+padding
-];
-if((nBounds[2]-nBounds[0])>minW)
-{
-    var nRatio=minW/(nBounds[2]-nBounds[0]);
-    doc.resizeImage(doc.width*nRatio);
-}
-if((nBounds[3]-nBounds[1])>minH) {
-    var nRatio=minH/(nBounds[3]-nBounds[1]);
-    doc.resizeImage(doc.width*nRatio);
-}
+                doc.resizeCanvas(resizeWidth + "px", resizeHeight + "px", AnchorPosition.TOPLEFT);
+            }
+            var cropc = getCropDimensions();
+            var nCrop = [
+                parseFloat(cropc[0].replace("px", "")) - padding + " px",
+                parseFloat(cropc[1].replace("px", "")) - padding + " px",
+                parseFloat(cropc[2].replace("px", "")) + padding + " px",
+                parseFloat(cropc[3].replace("px", "")) + padding + " px",
+            ];
+            doc.selection.deselect();
+            //alert(cropc.join('\r\n')+"\r\n"+[doc.width,doc.height].join("\r\n")+"\r\n"+nCrop.join("\r\n"));
+            doc.crop(nCrop);
+            doc.resizeCanvas(fw, fh, AnchorPosition.MIDDLECENTER);
+            var fillLayer = als.add();
+            fillLayer.name = "Fill";
+            doc.selection.selectAll();
+            var colorRef = new SolidColor();
+            colorRef.cmyk.cyan = "8";
+            colorRef.cmyk.magenta = "6";
+            colorRef.cmyk.yellow = "6";
+            colorRef.cmyk.black = "0";
+            doc.selection.fill(colorRef);
+            fillLayer.move(als[als.length - 1], ElementPlacement.PLACEAFTER);
+            doc.flatten();
 
-//doc.crop(nBounds);
-doc.resizeCanvas(fw, fh, AnchorPosition.MIDDLECENTER);
-//alert(crop.join(",")+":::"+nCrop+":::"+str.join(", "));
-
-var fillLayer = als.add();
-fillLayer.name="Fill";
-doc.selection.selectAll();
-var colorRef=new SolidColor;
-colorRef.cmyk.cyan="8";
-colorRef.cmyk.magenta="6";
-colorRef.cmyk.yellow="6";
-colorRef.cmyk.black="0";
-doc.selection.fill(colorRef);
-fillLayer.move(als[als.length-1],ElementPlacement.PLACEAFTER);
-doc.flatten();    
-
-            var nf = File(outFolder +"\\"+ app.activeDocument.name.split(".")[0]+".jpg");
+            var nf = File(outFolder + "\\" + app.activeDocument.name.split(".")[0] + ".jpg");
             var jpgSave = new JPEGSaveOptions();
-                jpgSave.embedColorProfile = true;
-                jpgSave.formatOptions = FormatOptions.STANDARDBASELINE;
-                jpgSave.matte = MatteType.NONE;
-                jpgSave.quality = 12;
-                app.activeDocument.saveAs(nf, jpgSave, true, Extension.LOWERCASE);
-                app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-                f.copy(outFolder + "\\" + f.name);
-                f.remove();
+            jpgSave.embedColorProfile = true;
+            jpgSave.formatOptions = FormatOptions.STANDARDBASELINE;
+            jpgSave.matte = MatteType.NONE;
+            jpgSave.quality = 12;
+            app.activeDocument.saveAs(nf, jpgSave, true, Extension.LOWERCASE);
+            app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+
+            f.remove();
         } catch (error) {
             //alert("Error:"+error.line+" "+error.message);
             app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-            
+
         }
     }
-    return true;}
-    
-    
+    return true;
+}
