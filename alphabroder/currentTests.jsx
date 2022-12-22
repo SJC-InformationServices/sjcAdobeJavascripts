813ffd043520ff7a929cbb363437832a40de71de
Kevin <kevin.noseworthy@stjoseph.com>
2022-12-22 11:37:02 -0500

    magicwand func file

--- alphabroder/currentTests.jsx
+++ alphabroder/currentTests.jsx
@@ -1,27 +1,4 @@
-function magicWand (x, y, t, a, c, s) {
-    try{
-    if (arguments.length < 2) return; // make sure have x,y
-    if (undefined == t) var t = 35; // set defaults of optional arguments
-    if (undefined == a) var a = true;
-    if (undefined == c) var c = false;
-    if (undefined == s) var s = false;
-    var desc = new ActionDescriptor();
-    var ref = new ActionReference();
-    ref.putProperty(charIDToTypeID('Chnl'), charIDToTypeID('fsel'));
-    desc.putReference(charIDToTypeID('null'), ref);
-    var positionDesc = new ActionDescriptor();
-    positionDesc.putUnitDouble(charIDToTypeID('Hrzn'), charIDToTypeID('#Rlt'), x); // in pixels
-    positionDesc.putUnitDouble(charIDToTypeID('Vrtc'), charIDToTypeID('#Rlt'), y);
-    desc.putObject(charIDToTypeID('T   '), charIDToTypeID('Pnt '), positionDesc);
-    desc.putInteger(charIDToTypeID('Tlrn'), t); // tolerance
-    desc.putBoolean(charIDToTypeID('Mrgd'), s); // sample all layers
-    if (!c) desc.putBoolean(charIDToTypeID("Cntg"), false); //  contiguous
-    desc.putBoolean(charIDToTypeID('AntA'), a); // anti-alias
-    executeAction(charIDToTypeID('setd'), desc, DialogModes.NO);
-    }catch(e){
-        return e;
-    }
-}
+
 function getCropDimensions(){
     for(var y=0;y<app.activeDocument.pathItems.length;y++)
     {
