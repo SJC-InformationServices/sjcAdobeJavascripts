328f13185920dbcb02481f641e388e1f579fd214
Kevin <kevin.noseworthy@stjoseph.com>
2023-06-28 15:26:18 -0400

    resize on ratio

--- canadiantire/cropAndCenter3500x5250jpgBkgroud.jsx
+++ canadiantire/cropAndCenter3500x5250jpgBkgroud.jsx
@@ -87,7 +87,8 @@ function cdnTire3500jpgPngBkgrd(log) {
             var getDim = getCropDimensions();
 
             var ratio = Math.min(minW / getDim.cropWidth, minH / getDim.cropHeight);
-            doc.resizeImage(w * ratio + "px", h * ratio + "px",300,ResampleMethod.BICUBIC);
+            //doc.resizeImage(w * ratio + "px", h * ratio + "px",300,ResampleMethod.BICUBIC);
+            doc.resizeImage(w * ratio + "px");
 
 
             var getDimB = getCropDimensions();
