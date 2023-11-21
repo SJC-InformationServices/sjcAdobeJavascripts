0e1344140bc36f6cda6d1ca3a30adf5c3e8abb00
Kevin <kevin.noseworthy@stjoseph.com>
2023-11-21 12:41:13 -0500

    server ps1

--- PhotoShop/server.ps1
+++ PhotoShop/server.ps1
@@ -1,5 +1,11 @@
 $appRef = New-Object -ComObject Photoshop.Application
-# Set the dialog mode to psDisplayNoDialogs
-$appRef.DisplayDialogs = 3
 
-$appRef.DoJavascript ("C:\Users\KevinNoseworthy\OneDrive - St Joseph Communications, Content Group\Documents\GitHub\photoshopscripts\PhotoShop\server.jsx")
+try 
+{
+    $docName = $appRef.ActiveDocument.Name
+} catch {
+    $appRef.DisplayDialogs = 3
+
+    $appRef.DoJavascript ("C:\Users\KevinNoseworthy\OneDrive - St Joseph Communications, Content Group\Documents\GitHub\photoshopscripts\PhotoShop\server.jsx")
+}   
+
