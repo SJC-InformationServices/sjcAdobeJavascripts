48887639d407b67afd06dd89b631adfb46534eaf
Kevin Noseworthy <kevin.noseworthy@stjoseph.com>
2024-05-06 10:51:12 -0400

    smteps logging

--- sobeys/convertToSMTEps.jsx
+++ sobeys/convertToSMTEps.jsx
@@ -1,9 +1,18 @@
-function convertToSMTEps(){
+function convertToSMTEps(log){
+
+    var current = new Date();
+    var cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+    var cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+    var dateTime = cDate + ' ' + cTime;
+    log.writeln("Start Sobeys EPS Convert" + dateTime);
+
+
 var inFolder = Folder("\\\\10.136.209.199\\Sobeys_Assets\\_HotFolders\\EpsClippingPath\\In");
 //var inFolder = Folder.selectDialog("Select Source Folder");
 var outFolder = Folder("\\\\10.136.209.199\\Sobeys_Assets\\_HotFolders\\EpsClippingPath\\Out");
 
 var files = inFolder.getFiles(/\.(psd|tif|jpg|)$/i);
+log.writeln("TotalFiles: " + files.length);
     
     for(var i=0;i<files.length;i++)
     {
@@ -36,5 +45,12 @@ var files = inFolder.getFiles(/\.(psd|tif|jpg|)$/i);
         return e;
     }
 }
+// Get the current date and time again at the end of processing all files
+var dcurrent = new Date();
+var dcDate = dcurrent.getFullYear() + '-' + (dcurrent.getMonth() + 1) + '-' + dcurrent.getDate();
+var dcTime = dcurrent.getHours() + ":" + dcurrent.getMinutes() + ":" + dcurrent.getSeconds();
+var ddateTime = dcDate + ' ' + dcTime;
+
+log.writeln("End: " + ddateTime);
 return true;
 }
